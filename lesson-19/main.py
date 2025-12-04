313f58ca54d11903e70c329bc399cf93315b36a9
Matnazar <matnazarmatnazarov3@gmail.com>
2025-12-04 16:49:49 +0500

    lesson 19 done

--- lesson-19/main.py
+++ lesson-19/main.py
@@ -1,34 +1,16 @@
-# ================== OBJECT DETECTION: OpenCV + MobileNet SSD ==================
 import cv2
-import numpy as np
 
-# Oldindan o‘qitilgan model va konfiguratsiya (COCO sinflari uchun)
-CONFIG_PATH = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
-WEIGHTS_PATH = "frozen_inference_graph.pb"
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# COCO datasetidagi 80 sinf nomlari
-classNames = []
-with open("coco.names", "r") as f:
-    classNames = f.read().strip().split("\n")
+img = cv2.imread("test.png")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Image", gray)
 
-# Model yuklash
-net = cv2.dnn_DetectionModel(WEIGHTS_PATH, CONFIG_PATH)
-net.setInputSize(320, 320)
-net.setInputScale(1.0 / 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-# Tasvir yuklash
-img = cv2.imread("test.jpg")   # <-- bu yerga o‘z rasm faylingizni qo‘ying
-classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-# Natija chizish
-if len(classIds) != 0:
-    for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-        cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-        cv2.putText(img, f"{classNames[classId-1].upper()} {round(confidence*100,1)}%",
-                    (box[0]+10, box[1]+30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
-
-cv2.imshow("Obyekt aniqlash", img)
+cv2.imshow("Yuz aniqlash", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
